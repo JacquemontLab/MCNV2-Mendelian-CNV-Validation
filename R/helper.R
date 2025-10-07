ba9b9d7a767c765d3c09367516e14473b53f4f85
Audrey Lemacon <audrey.lemacon.hsj@ssss.gouv.qc.ca>
2025-10-07 18:11:03 -0400

    add wrapper for main functions (gene annotation and inheritance calculation)

--- R/helper.R
+++ R/helper.R
@@ -1,5 +1,5 @@
 #' @export
-check_input_file <- function(filepath, file_type = c("cnv", "ped", "annot"), sep = "\t") {
+check_input_file <- function(filepath, file_type = c("cnv", "ped", "annot","prb", "preproc"), sep = "\t") {
 	file_type <- match.arg(file_type)
 	
 	if (!file.exists(filepath)) {
@@ -28,9 +28,40 @@ check_input_file <- function(filepath, file_type = c("cnv", "ped", "annot"), sep
 				iid = c("iid", "individual", "sampleid", "childid", "id")
 			),
 			unordered = list(
-				fid = c("fid", "family", "familyid", "fam_id"),
 				father = c("pid", "father", "dad", "fatherid"),
 				mother = c("mid", "mother", "mom", "motherid")
+			)
+		),
+		prb = list(
+			ordered = list(
+				chr   = c("chr", "chrom", "seqnames"),
+				start = c("start", "begin", "pos_start"),
+				end   = c("end", "stop", "pos_end")
+			),
+			unordered = list()
+		),
+		preproc = list(
+			ordered = list(
+				cnvid   = c("cnv_id")
+			),
+			unordered = list(
+				chr   = c("chr", "chrom", "seqnames","chr_child", "chrom_child", "seqnames_child"),
+				start = c("start", "begin", "pos_start","start_child", "begin_child", "pos_start_child"),
+				end   = c("end", "stop", "pos_end","end_child", "stop_child", "pos_end_child"),
+				sample = c("sampleid", "sample_id", "id_sample","sampleid_child"),
+				type   = c("type", "svtype", "variant_type","type_child"),
+				length = c("size","length","size_child"),
+				length_range = c("size_range","length_range"),
+				exon_overlap = c("exon_overlap"),
+				transcript_overlap = c("transcript_overlap","transcript_bp_overlap"),
+				geneid = c("gene_id"),
+				overlap_pr = c("overlappr"),
+				#biotype = c("biotype"),
+				#cnv_inherit = c("inheritance_by_cnv"),
+				gene_inherit = c("inheritance_by_gene"),
+				loeuf = c("loeuf"),
+				overlap_father = c("overlap_cnv_father"),
+				overlap_mother = c("overlap_cnv_mother")
 			)
 		),
 		other = list(
@@ -78,3 +109,43 @@ check_input_file <- function(filepath, file_type = c("cnv", "ped", "annot"), sep
 	return(list(status = TRUE, msg = "✅ File check passed!"))
 }
 
+#' @export
+annotate <- function(cnvs_file, prob_regions_file, output_file, 
+										 genome_version = 38, bedtools_path){
+	
+	gene_annotation_script <- system.file("python",
+																				"gene_annotation.py", 
+																				package = "MCNV2")
+	gene_resource_file <- system.file("resources",
+																		"gene_resources.tsv", 
+																		package = "MCNV2")
+	
+	cmd = paste("python3", gene_annotation_script, "--cnv", cnvs_file,
+							"--gene_resource", gene_resource_file, "--prob_regions", prob_regions_file,
+							"--out", output_file, 
+							"--genome_version", genome_version, "--bedtools_path", bedtools_path)
+	ret <- system(command = cmd, intern = FALSE)
+	
+	return(ret)
+}
+
+
+compute_cnv_inheritance <- function(){
+	return(NULL)
+}
+
+compute_gene_inheritance <- function(){
+	return(NULL)
+}
+
+#' @export
+compute_inheritance <- function(){
+	print(system.file("python",
+										"gene_annotation.py", 
+										package = "MCNV2"))
+	
+	gene_inheritance <- compute_gene_inheritance()
+	cnv_inheritance <- compute_cnv_inheritance()
+
+	return(0)
+}
